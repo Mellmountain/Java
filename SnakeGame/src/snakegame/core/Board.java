1f328be256909e863d76343a8984a2cac6e8a7da
Jonas Mellberg <jonas@gmail.com>
2015-06-10 02:19:54 +0200

    Added some stuff

--- SnakeGame/src/snakegame/core/Board.java
+++ SnakeGame/src/snakegame/core/Board.java
@@ -1,5 +1,180 @@
 package snakegame.core;
 
-public class Board {
-	String s = "made som changes";
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+import pathfinding.core.Node;
+import pathfinding.core.Position;
+import snakegame.core.Board.BoardPlayState;
+import snakegame.core.Snake.Direction;
+
+public class Board 
+{	
+	private int _xTiles;
+	private int _yTiles;
+	
+	private ArrayList<Food> _food;
+	private ArrayList<Snake> _players;
+	
+	private BoardPlayState _boardPlayState;
+	private Random _random;
+	
+	public enum BoardPlayState
+	{
+		PLAYING, GAME_OVER
+	}
+	
+	public Board(int sizeX, int sizeY, int numberOfPlayers)
+	{
+		_xTiles = sizeX;
+		_yTiles = sizeY;
+		
+		Snake player1 = new Snake(new Position(2, 3), Direction.RIGHT, Color.BLUE, "BlueSnake");
+		Snake player2 = new Snake(new Position(8, 8), Direction.LEFT, Color.GREEN, "GreenSnake");
+		_players = new ArrayList<Snake>();
+		_players.add(player1);
+		_players.add(player2);
+				
+		_food = new ArrayList<Food>();
+		
+		_boardPlayState = BoardPlayState.PLAYING;
+		
+		_random = new Random(System.currentTimeMillis());
+		generateNewFood();
+	}
+	
+	public void update() 
+	{
+		if(hasAllPlayersDied())
+			_boardPlayState = BoardPlayState.GAME_OVER;
+		
+		if(_boardPlayState != BoardPlayState.GAME_OVER)
+		{
+			for(Snake player : _players)
+			{
+				// Don't update dead players
+				if(player.isAlive())
+				{
+					player.update();
+					if(hasCollidedWithWall(player) || hasCollidedWithOpponent(player) || hasCollidedWithSelf(player))
+						player.setAlive(false);
+					
+					if(hasArrivedAtFood(player))
+					{
+						player.grow(getFoodAt(player.getBody().get(0)));
+						generateNewFood();
+					}
+				}
+			}			
+		}
+	}
+
+	private void generateNewFood() 
+	{
+		//this is bad if we want to have more than one food on board
+		//we should probably check so we dont generate food on top of a player (will be bad for AI)
+		if(_food.size() > 0)
+			_food.remove(0);
+		_food.add(new Food(new Position(_random.nextInt(_xTiles - 1), _random.nextInt(_yTiles - 1)), _random.nextInt(4) + 1));
+	}
+
+	public ArrayList<Snake> getPlayers()
+	{
+		return _players;
+	}
+
+	public ArrayList<BoardEntity> getBoardState()
+	{
+		ArrayList<BoardEntity> boardState = new ArrayList<BoardEntity>();
+		boardState.addAll(_players);
+		boardState.addAll(_food);
+		return boardState;
+	}
+	
+	private boolean hasAllPlayersDied() 
+	{
+		boolean allPlayersAreDead = true;
+		for(Snake player : _players)
+		{
+			if(player.isAlive())
+			{
+				allPlayersAreDead = false;
+				break;
+			}
+		}
+		return allPlayersAreDead;
+	}
+	
+	private boolean hasCollidedWithWall(Snake player)
+	{
+		Position headPosition = player.getBody().get(0);
+		if(headPosition.x < 0 
+				|| headPosition.x > _xTiles
+				|| headPosition.y < 0
+				|| headPosition.y > _yTiles)
+		{
+			System.out.println(player.getName() + "has collided with wall");
+			return true;
+		}
+		return false;
+	}
+
+	private boolean hasCollidedWithOpponent(Snake player) {
+		Position headPosition = player.getBody().get(0);
+		for(Snake opponent : _players)
+		{
+			//bad check!? This will trigger if two snakes collide head on! 
+			//We don't want to compare against out own head
+			if(headPosition.equals(opponent.getBody().get(0)))
+					continue;
+			for(Position opponentBodyPosition : opponent.getBody())
+			{
+				if(headPosition.equals(opponentBodyPosition))
+					return true;
+			}
+		}
+		return false;
+	}
+	private boolean hasCollidedWithSelf(Snake player) {
+		Position headPosition = player.getBody().get(0);
+		for(Position bodyPosition : player.getBodyWithoutHead())
+		{
+			if(headPosition.equals(bodyPosition))
+				return true;
+		}
+		return false;
+	}
+	
+	private boolean hasArrivedAtFood(Snake player)
+	{
+		Position headPosition = player.getBody().get(0);
+		for(Food food : _food)
+		{
+			if(headPosition.x == food.getPosition().get(0).x  && headPosition.y == food.getPosition().get(0).y)
+				{
+					System.out.println(player.getName() + "found food @" + player.getPosition().get(0).print() + " score:" + food.getGrowFactor());
+					return true;
+				}
+		}
+		return false;
+	}
+	
+	private Food getFoodAt(Position position) {
+		Food foodAtPosition = null;
+		for(Food food : _food)
+		{
+			if(position.equals(food.getPosition().get(0)))
+			{
+				foodAtPosition = food;
+				break;
+			}
+		}
+		return foodAtPosition;
+	}
+
+	public BoardPlayState getBoardPlayState() {
+		return _boardPlayState;
+	}
 }
--- SnakeGame/src/snakegame/core/Snake.java
+++ SnakeGame/src/snakegame/core/Snake.java
@@ -1,5 +1,156 @@
 package snakegame.core;
 
-public class Snake {
+import java.awt.Color;
+import java.util.ArrayList;
 
+import pathfinding.core.*;
+
+public class Snake extends BoardEntity 
+{
+
+	private Direction _direction;
+	private ArrayList<Position> _body;
+	private Color _color;
+	private int _score;
+	private int _growFactor = 0;
+	private boolean _isAlive;
+	private String _name;
+	
+	public enum Direction
+	{
+		UP, DOWN, LEFT , RIGHT
+	}
+	
+	public Snake(Position startPos, Direction direction, Color color, String name)
+	{
+		_direction = direction;
+		_color = color;
+		_body = new ArrayList<Position>();
+		_body.add(0, startPos);		
+		_growFactor = 2;
+		_isAlive = true;
+		_name = name;
+	}
+	
+	public void update()
+	{
+		if(_growFactor == 0)
+			_body.remove(_body.size() - 1);
+		else
+			_growFactor--;
+		
+		_body.add(0, getNewPosition(_body.get(0), _direction));
+		System.out.println(printSnakeBody());
+	}
+	
+	
+
+	public void grow(Food food)
+	{
+		if(food != null)
+			{
+				_growFactor = food.getGrowFactor();
+				_score = food.getGrowFactor();
+			}
+	}
+	
+	private Position getNewPosition(Position currentPos, Direction direction)
+	{
+		Position newPosition;
+		switch (direction)
+		{
+			case UP:			
+				newPosition = new Position(currentPos.x, currentPos.y - 1);
+				break;
+			case DOWN:
+				newPosition = new Position(currentPos.x, currentPos.y + 1);
+				break;
+			case LEFT:
+				newPosition = new Position(currentPos.x - 1, currentPos.y);
+				break;
+			case RIGHT:
+				newPosition = new Position(currentPos.x + 1, currentPos.y);
+				break;
+			default:
+				// will never go here ;)
+				newPosition = new Position(0, 0);
+		}
+		return newPosition;
+	}
+
+	public ArrayList<Position> getBody() {
+		return _body;
+	}
+
+	public void setDirection(Direction direction) {
+		// Make sure we cant make an illegal move maybe Board should do this?
+		if(_direction == Direction.UP && direction == Direction.DOWN)
+			return;
+		if(_direction == Direction.DOWN && direction == Direction.UP)
+			return;
+		if(_direction == Direction.LEFT && direction == Direction.RIGHT)
+			return;
+		if(_direction == Direction.RIGHT && direction == Direction.LEFT)
+			return;
+		_direction = direction;
+	}
+
+	public int getScore() {
+		return _score;
+	}
+
+	public void setScore(int _score) {
+		this._score = _score;
+	}
+
+	public boolean isAlive() {
+		return _isAlive;
+	}
+
+	public void setAlive(boolean isAlive) {
+		this._isAlive = isAlive;
+	}
+
+	
+	@Override
+	public BoardEntityType getEntityType() {
+		return BoardEntityType.PLAYER;
+	}
+
+	@Override
+	public ArrayList<Position> getPosition() {
+		return _body;
+	}
+
+	@Override
+	public Color getEntityColor() {		
+		return _color;
+	}
+	
+	public String getName()
+	{
+		return _name;
+	}
+	
+	private char[] printSnakeBody() {
+		String strBody = "[H]";
+		for(Position pos : _body)
+		{
+			strBody += "[" + pos.x + "," + pos.y + "]";
+		}
+		strBody += "[T]-" + _name;
+		return strBody.toCharArray();
+	}
+
+	public boolean growing() {
+		return _growFactor > 0;
+	}
+
+	public ArrayList<Position> getBodyWithoutHead() {
+		ArrayList<Position> bodyNoHead = (ArrayList<Position>) _body.clone();;
+		bodyNoHead.remove(0);
+		return bodyNoHead;
+	}
+	
+	
 }
